a15e3db3d9b785a8ec245ceaeb24242ed77bc8f1
mansibabbar <mansib@carbyne911.com>
2021-10-25 14:43:50 +0530

    UserAppTest refactored

--- src/test/java/io/thundra/demo/localstack/integration/UserAppTest.java
+++ src/test/java/io/thundra/demo/localstack/integration/UserAppTest.java
@@ -1,26 +1,13 @@
 package io.thundra.demo.localstack.integration;
 
-import io.thundra.agent.lambda.localstack.LambdaServer;
 import io.thundra.demo.localstack.LocalStackTest;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public abstract class UserAppTest extends LocalStackTest {
-    @BeforeAll
-    static void beforeAll() throws Exception {
-        LambdaServer.start();
-    }
-
-    @AfterAll
-    static void afterAll() throws Exception {
-        LambdaServer.stop();
-    }
-
     @Test
     public void testCreateNewRequest() {
-        assertThat(true);
+        assertEventually(() -> assertThat(true));
     }
 }
