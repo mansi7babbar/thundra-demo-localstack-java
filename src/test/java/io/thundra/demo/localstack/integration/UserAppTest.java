830110bcc94ec9688c283d7dfd757e7e1d4074ec
mansibabbar <mansib@carbyne911.com>
2021-10-25 15:02:43 +0530

    Revert "Testing execution of tests"
    This reverts commit 8650acb8fcb51e60734014c42d2d0dd4656308c3.

--- src/test/java/io/thundra/demo/localstack/integration/UserAppTest.java
+++ src/test/java/io/thundra/demo/localstack/integration/UserAppTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public abstract class UserAppLocalStackTest extends LocalStackTest {
+public abstract class UserAppTest extends LocalStackTest {
     @Test
     public void testCreateNewRequest() {
         assertThat(true);
