8acbda5a55e88c3271b04e1a0ddee16529bca9de
mansibabbar <mansib@carbyne911.com>
2021-10-25 14:22:03 +0530

    Added basic test

---
+++ src/test/java/io/thundra/demo/localstack/UserAppTest.java
@@ -0,0 +1,25 @@
+package io.thundra.demo.localstack;
+
+import io.thundra.agent.lambda.localstack.LambdaServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public abstract class UserAppTest {
+    @BeforeAll
+    static void beforeAll() throws Exception {
+        LambdaServer.start();
+    }
+
+    @AfterAll
+    static void afterAll() throws Exception {
+        LambdaServer.stop();
+    }
+
+    @Test
+    public void testCreateNewRequest() {
+        assertThat(true);
+    }
+}
