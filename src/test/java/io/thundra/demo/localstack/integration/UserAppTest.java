473301708beca9a8044319523fee53e5e2e6e76b
mansibabbar <mansib@carbyne911.com>
2021-10-26 02:10:18 +0530

    Testing

--- src/test/java/io/thundra/demo/localstack/integration/UserAppTest.java
+++ src/test/java/io/thundra/demo/localstack/integration/UserAppTest.java
@@ -12,13 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class UserAppTest extends LocalStackTest {
     @Test
     public void testCreateNewRequest() throws IOException {
-        assertEventually(() -> {
-            try {
-                ResponseEntity<User> responseEntity = get(lambdaUrl, User.class);
-                assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.SC_OK);
-            } catch (IOException e) {
-                throw new AssertionError(e);
-            }
-        });
+        ResponseEntity<User> responseEntity = get(lambdaUrl, User.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.SC_OK);
     }
 }
