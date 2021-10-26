31cee0ef7400ab6b4e7e0e81788dc01f778cd2a7
mansibabbar <mansib@carbyne911.com>
2021-10-26 15:24:17 +0530

    Updated string entity

--- src/test/java/io/thundra/demo/localstack/integration/UserAppTest.java
+++ src/test/java/io/thundra/demo/localstack/integration/UserAppTest.java
@@ -5,6 +5,7 @@ import io.thundra.demo.localstack.LocalStackTest;
 import io.thundra.demo.localstack.model.User;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.junit.jupiter.api.Test;
 
@@ -18,19 +19,19 @@ public class UserAppTest extends LocalStackTest {
 
     @Test
     public void testGetUserRequest() throws IOException {
-        ResponseEntity<User> getUser = get(lambdaUrl + "/1", User.class);
-        System.out.println("MB - Testing getUserRequest " + getUser);
-        assertThat(getUser.getStatusCode()).isEqualTo(HttpStatus.SC_OK);
+        ResponseEntity<User> getUserResponse = get(lambdaUrl + "/1", User.class);
+        System.out.println("MB - Testing getUserRequest " + getUserResponse);
+        assertThat(getUserResponse.getStatusCode()).isEqualTo(HttpStatus.SC_OK);
     }
 
     @Test
     public void testPostUserRequest() throws IOException {
-        HttpPost request = new HttpPost(lambdaUrl);
+        HttpPost postUserRequest = new HttpPost(lambdaUrl);
         User defaultUser = new User(1, "Mansi", "abc", "xyz");
-        StringEntity userEntity = new StringEntity(mapper.writeValueAsString(defaultUser));
-        request.setEntity(userEntity);
-        System.out.println("MB - Testing postUserRequest " + request);
-        ResponseEntity<User> postUser = post(request, User.class);
-        assertThat(postUser.getStatusCode()).isEqualTo(HttpStatus.SC_OK);
+        StringEntity userEntity = new StringEntity(mapper.writeValueAsString(defaultUser), ContentType.APPLICATION_JSON);
+        postUserRequest.setEntity(userEntity);
+        System.out.println("MB - Testing postUserRequest " + postUserRequest);
+        ResponseEntity<User> postUserResponse = post(postUserRequest, User.class);
+        assertThat(postUserResponse.getStatusCode()).isEqualTo(HttpStatus.SC_OK);
     }
 }
