e543c9c36b32f804a76e754d254e8ef03e7814de
mansibabbar <mansib@carbyne911.com>
2021-10-26 19:56:51 +0530

    Fixed compilation issues

--- src/main/java/io/thundra/demo/localstack/service/UserService.java
+++ src/main/java/io/thundra/demo/localstack/service/UserService.java
@@ -1,28 +1,23 @@
 package io.thundra.demo.localstack.service;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.thundra.demo.localstack.model.User;
-import static io.thundra.demo.localstack.service.ClientBuilder.*;
+
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
-import java.util.Map;
+import io.thundra.demo.localstack.model.User;
+
+import static io.thundra.demo.localstack.service.ClientBuilder.buildDynamoDB;
 
 public class UserService {
- 
-    public static final String USERS_TABLE_NAME = System.getenv("USERS_TABLE_NAME");
 
-    private final ObjectMapper mapper = new ObjectMapper();
+    public static final String USERS_TABLE_NAME = System.getenv("USERS_TABLE_NAME");
 
     private final AmazonDynamoDB dynamoDB;
     private final DynamoDBMapper dynamoDBMapper;
- 
+
     public UserService() {
         this.dynamoDB = buildDynamoDB();
         this.dynamoDBMapper = buildDynamoDBMapper(dynamoDB);
     }
-    
 
     private DynamoDBMapper buildDynamoDBMapper(AmazonDynamoDB dynamoDB) {
         return new DynamoDBMapper(dynamoDB, DynamoDBMapperConfig.builder()
@@ -32,19 +27,17 @@ public class UserService {
     }
 
     public Boolean insertUser(User user) {
-        try{
+        try {
             dynamoDBMapper.save(user);
             return true;
-        }catch(Exception e){
-            System.out.println("Exception occured in insert user"+e.getMessage())
+        } catch (Exception e) {
+            System.out.println("Exception occurred while inserting user " + e.getMessage());
             return false;
 
         }
-        return userRepository.insertItem(user);
     }
 
     public User getUser(int id) {
-        return dynamoDBMapper.load(User.class, requestId);
-
+        return dynamoDBMapper.load(User.class, id);
     }
 }
