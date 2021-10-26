8db8251de8b9e6da9fc0f7986c3f8532dfa7c0a5
mansibabbar <mansib@carbyne911.com>
2021-10-26 16:07:32 +0530

    Updated User get and post handlers

--- src/main/java/io/thundra/demo/localstack/handler/UserApp.java
+++ src/main/java/io/thundra/demo/localstack/handler/UserApp.java
@@ -5,12 +5,10 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
 import io.thundra.demo.localstack.model.User;
 import io.thundra.demo.localstack.service.UserService;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.json.simple.parser.JSONParser;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,48 +22,37 @@ public class UserApp implements RequestHandler<APIGatewayProxyRequestEvent, APIG
     }};
     private final UserService userService = new UserService();
     ObjectMapper mapper = new ObjectMapper();
-    JSONParser parser = new JSONParser();
-    Gson gson = new Gson();
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
         try {
             logger.info("Request --> " + request);
             if ("/user".equals(request.getPath()) && "POST".equals(request.getHttpMethod())) {
-//                JSONObject requestBody = (JSONObject) parser.parse(request.getBody());
-//                User user = gson.fromJson(requestBody.toString(), User.class);
                 User user = mapper.readValue(request.getBody(), User.class);
                 Boolean insertUserResponse = userService.insertUser(user);
-                return new APIGatewayProxyResponseEvent().
-                        withStatusCode(200).
-                        withHeaders(headers).
-                        withBody(mapper.writeValueAsString(user));
-//                if (insertUserResponse) {
-//                    return new APIGatewayProxyResponseEvent().withStatusCode(200)
-//                            .withHeaders(headers)
-//                            .withBody(mapper.writeValueAsString(user));
-//                } else {
-//                    return new APIGatewayProxyResponseEvent().withStatusCode(400)
-//                            .withHeaders(headers)
-//                            .withBody("Failed to create user!");
+                if (insertUserResponse) {
+                    return new APIGatewayProxyResponseEvent().
+                            withStatusCode(200).
+                            withHeaders(headers).
+                            withBody(mapper.writeValueAsString(user));
+                } else {
+                    return new APIGatewayProxyResponseEvent().withStatusCode(400)
+                            .withHeaders(headers)
+                            .withBody("Failed to create user!");
+                }
             } else if (Pattern.matches("/user/\\d*", request.getPath()) && "GET".equals(request.getHttpMethod())) {
                 Map<String, String> pathParameters = request.getPathParameters();
                 int userID = Integer.parseInt(pathParameters.get("userid"));
-//                User getUserResponse = userService.getUser(userID);
-                User defaultUser = new User(1, "Mansi", "abc", "xyz");
-                return new APIGatewayProxyResponseEvent().
-                        withStatusCode(200).
-                        withHeaders(headers).
-                        withBody(mapper.writeValueAsString(defaultUser));
-//                if (getUserResponse != null) {
-//                    return new APIGatewayProxyResponseEvent().withStatusCode(200)
-//                            .withHeaders(headers)
-//                            .withBody(mapper.writeValueAsString(getUserResponse));
-//                } else {
-//                    return new APIGatewayProxyResponseEvent().withStatusCode(200)
-//                            .withHeaders(headers)
-//                            .withBody("Failed to get user!");
-//                }
+                User getUserResponse = userService.getUser(userID);
+                if (getUserResponse != null) {
+                    return new APIGatewayProxyResponseEvent().withStatusCode(200)
+                            .withHeaders(headers)
+                            .withBody(mapper.writeValueAsString(getUserResponse));
+                } else {
+                    return new APIGatewayProxyResponseEvent().withStatusCode(400)
+                            .withHeaders(headers)
+                            .withBody("Failed to get user!");
+                }
             } else {
                 return new APIGatewayProxyResponseEvent().
                         withStatusCode(404);
